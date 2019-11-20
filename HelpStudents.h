2fc3cd60dac76a320bd2b4e757cbb32baa3b01e8
Çağrı <gokceca@itu.edu.tr>
2019-11-20 09:32:39 +0300

    Initial commit

---
+++ HelpStudents.h
@@ -0,0 +1,22 @@
+#ifndef CMPE250_ASSIGNMENT3_HELPSTUDENTS_H
+#define CMPE250_ASSIGNMENT3_HELPSTUDENTS_H
+#include <vector>
+
+using namespace std;
+
+
+class HelpStudents{
+
+public:
+    HelpStudents(int  N, int  M, int K, vector < pair< pair <int,int> , int > > ways);
+    long long int firstStudent();
+    long long int secondStudent();
+    long long int thirdStudent();
+    long long int fourthStudent();
+    long long int fifthStudent();
+
+    // YOU CAN ADD YOUR HELPER FUNCTIONS AND MEMBER FIELDS
+
+};
+
+#endif //CMPE250_ASSIGNMENT3_HELPSTUDENTS_H
